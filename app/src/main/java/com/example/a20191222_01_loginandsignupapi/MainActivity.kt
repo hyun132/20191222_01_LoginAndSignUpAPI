5402b4858c08eea3344aaf8349fd733f4d26a5f3
hyun <rlaalgus132@naver.com>
2019-12-22 15:47:22 +0900

    목록을 분석해서 => 데이터 객체로 변환 => 배열에 담아주기

--- app/src/main/java/com/example/a20191222_01_loginandsignupapi/MainActivity.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a20191222_01_loginandsignupapi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.a20191222_01_loginandsignupapi.datas.BlackListData
 import com.example.a20191222_01_loginandsignupapi.datas.User
 import com.example.a20191222_01_loginandsignupapi.utils.ConnectServer
@@ -38,6 +39,29 @@ class MainActivity : BaseActivity() {
         ConnectServer.getRequestBlackList(mContext,object :ConnectServer.JsonResponseHandler{
             override fun onResponse(json: JSONObject) {
                 Log.d("블랙리스트목록응답",json.toString())
+
+                val code = json.getInt("code")
+
+                if(code ==200){
+
+                    val data = json.getJSONObject("data")
+                    val black_lists = data.getJSONArray("black_lists")
+
+//                    블랙리스트 : 2개 => 0, 1
+                    for (i in 0..black_lists.length()-1)
+
+//                        JSONArray 내부의 i번째 JSONObject를 추출
+//                        val tempJson = black_lists.getJSONObject(i)
+//                    뽑아낸 jsonobject => BlackListData클래스로 변경(리스트뷰ㅜ에서 활용가능)
+//                    val tempData = BlackListData.getBlackListDataFromJson(tempJson)
+//                    blackList.add(tempData)
+
+                        blackList.add(BlackListData.getBlackListDataFromJson(black_lists.getJSONObject(i)))
+
+                }else{
+                    val message = json.getString("message")
+                    runOnUiThread { Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show() }
+                }
             }
 
         })
--- app/src/main/java/com/example/a20191222_01_loginandsignupapi/datas/BlackListData.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/datas/BlackListData.kt
@@ -1,9 +1,24 @@
 package com.example.a20191222_01_loginandsignupapi.datas
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class BlackListData(phone:String, title:String ,content:String):Serializable {
     var phoneNum = phone
     var title = title
     var content =content
+
+    constructor() : this("모름","미입력","미작성")
+
+    companion object {
+        fun getBlackListDataFromJson(json:JSONObject):BlackListData{
+            val bld=BlackListData()
+
+            bld.phoneNum = json.getString("phone_num")
+            bld.title = json.getString("title")
+            bld.content = json.getString("content")
+
+            return bld
+        }
+    }
 }
