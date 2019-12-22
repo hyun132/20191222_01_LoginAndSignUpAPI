7018059b49b3c2bc5caa0e2c9939aff5346e4a53
hyun <rlaalgus132@naver.com>
2019-12-22 11:58:06 +0900

    회원가입 API 호출 기능 작성

--- app/src/main/java/com/example/a20191222_01_loginandsignupapi/LoginActivity.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.a20191222_01_loginandsignupapi
 
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
@@ -12,7 +14,10 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-
+        signUpBtn.setOnClickListener {
+            val intent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun setValues() {
