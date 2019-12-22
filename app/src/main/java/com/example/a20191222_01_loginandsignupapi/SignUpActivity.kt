9b5af9a6abdb14b1ad1b7e6e08feadcd6ff57a15
hyun <rlaalgus132@naver.com>
2019-12-22 11:20:24 +0900

    회원가입화면 초안

---
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/SignUpActivity.kt
@@ -0,0 +1,24 @@
+package com.example.a20191222_01_loginandsignupapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
