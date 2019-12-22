09557df21755a59095e61d87d992da4182e510db
hyun <rlaalgus132@naver.com>
2019-12-22 12:45:08 +0900

    code 값에 따른 분기처리(서버의 응답에 따른 처리)

--- app/src/main/java/com/example/a20191222_01_loginandsignupapi/SignUpActivity.kt
+++ app/src/main/java/com/example/a20191222_01_loginandsignupapi/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.example.a20191222_01_loginandsignupapi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.a20191222_01_loginandsignupapi.utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -26,6 +27,19 @@ class SignUpActivity : BaseActivity() {
             ConnectServer.putRequestSignUp(mContext,inputId,inputPw,inputName,inputPhone, object : ConnectServer.JsonResponseHandler{
                 override fun onResponse(json: JSONObject) {
                     Log.d("회원가입 응답", json.toString())
+
+//                    서버가 내려주는 code값이 몇인지?확인
+                    val code = json.getInt("code")
+                    Log.d("회원가입코드", "${code}")
+
+                    runOnUiThread {  if (code ==200){
+                        Toast.makeText(mContext,"회원가입 성공입니다.",Toast.LENGTH_SHORT).show()
+                        finish()
+                    }else{
+                        Toast.makeText(mContext,"회원가입 실패입니다.",Toast.LENGTH_SHORT).show()
+                    } }
+
+
                 }
             })
         }
